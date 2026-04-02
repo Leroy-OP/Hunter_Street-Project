978320572c45a5f1931a690ecc8cfc896300dde9
leroy <kerettsleroy@gmail.com>
2026-04-02 20:39:38 +0300

    Second Commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from "./components/navbar.vue"
+</script>
+
+<template>
+    <v-app>
+      <Navbar/>
+      <v-main>
+        <transition name="fade" mode="out-in">
+          <router-view />
+        </transition>
+      </v-main>
+    </v-app>
+</template>
